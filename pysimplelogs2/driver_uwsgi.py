3930fca3e0e84015db477d1ee552c37d92511a47
Михаил Мальков <m.malkov@bars-open.ru>
2016-03-24 19:02:51 +0300

    Сообщения об ошибках связи с сервером при работе с мулами

--- pysimplelogs2/driver_uwsgi.py
+++ pysimplelogs2/driver_uwsgi.py
@@ -2,18 +2,29 @@
 # noinspection PyUnresolvedReferences
 import uwsgidecorators
 
+import logging
+
 __author__ = 'viruzzz-kun'
+
+
+error_logger = logging.getLogger('PySimplelogs')
 
 
 @uwsgidecorators.mulefunc
 def send(url, data, timeout=10):
     import requests
-    requests.post(
-        url,
-        data=data,
-        headers={
-            'Content-type': 'application/json',
-            'Accept': 'application/json; text/plain',
-        },
-        timeout=timeout,
-    )
+    try:
+        result = requests.post(
+            url,
+            data=data,
+            headers={
+                'Content-type': 'application/json',
+                'Accept': 'application/json; text/plain',
+            },
+            timeout=timeout,
+        )
+        if result.status_code >= 400:
+            result.raise_for_status()
+    except:
+        error_logger.exception(u'Error communicating with Simplelogs server')
+
